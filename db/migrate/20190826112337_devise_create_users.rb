2f698f6fb035118e737b8e1ba74b672ae18c0558
kurachitoshiaki <aru0220kuria0214@gmail.com>
2019-10-26 22:25:43 +0900

    Update 20190826112337_devise_create_users.rb

--- db/migrate/20190826112337_devise_create_users.rb
+++ db/migrate/20190826112337_devise_create_users.rb
@@ -5,8 +5,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
+      t.text :text
+      t.text :image
       t.string :encrypted_password, null: false, default: ""
-      t.string :nickname, null: false, default: ""
+      t.integer :users, :height, null: false, default: ""
+      t.integer :users, :weight, null: false, default: ""
+      t.integer :users, :BMI, null: false, default: ""
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
