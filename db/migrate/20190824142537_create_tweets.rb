6696a8cf7fc52c55033964c170ad580f15beb495
kurachitoshiaki <aru0220kuria0214@gmail.com>
2019-10-26 22:25:40 +0900

    Update 20190824142537_create_tweets.rb

--- db/migrate/20190824142537_create_tweets.rb
+++ db/migrate/20190824142537_create_tweets.rb
@@ -1,9 +1,10 @@
-class CreateTweets < ActiveRecord::Migration[5.2]
+class CreateTrainings < ActiveRecord::Migration[5.2]
   def change
-    create_table :tweets do |t|
-      t.text        :text
-      t.string      :user_id
-      t.timestamps null: true
+    create_table :trainings do |t|
+      t.string :name
+      t.integer :MET
+      t.integer :time
+      t.integer :kcal
     end
   end
 end
